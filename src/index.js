33997068cbf56536006203f7c081463553224252
Behnam Azimi <bhnmzm@gmail.com>
2021-10-20 18:21:00 +0300

    pdate styles and fix options

--- src/index.js
+++ src/index.js
@@ -1,120 +1,135 @@
-const Board = require("./board");
+const Board = require('./board')
 
 class OthelloGame {
+  defaultOptions = {
+    width: 6,
+    cellMaxWidth: 80,
+    selector: 'othello',
+    cellBGColor: '#43a047',
+  }
 
-    defaultOptions = {
-        width: 8,
-        cellMaxWidth: 80,
-        selector: "othello",
-        cellBGColor: "#43a047",
-    };
+  constructor(options = {}) {
+    if (options && typeof options === 'object')
+      this.options = { ...this.defaultOptions, ...options }
 
-    constructor(options = {}) {
-        if (options && typeof options === "object")
-            this.options = {...this.defaultOptions, ...options};
+    this.board = new Board(this.options.width)
 
-        this.board = new Board(this.options.width);
+    this.initGame()
+  }
 
-        this.initGame();
+  initGame() {
+    const { selector = 'othella' } = this.options
+
+    this.elm = document.getElementById(selector)
+
+    if (!this.elm) throw new Error(`Element not found for id ${selector}`)
+
+    for (let y = 0; y < this.options.width; y++) {
+      const boardRowElm = this.createRowElm(y)
+      for (let x = 0; x < this.options.width; x++) {
+        const cell = this.board.getCell(x, y)
+
+        const boardCellElm = this.createCellElm(x, y)
+        boardCellElm.appendChild(
+          this.createNutElm(
+            cell.owner,
+            this.board.isValidMove(x, y),
+            this.board.turn,
+          ),
+        )
+
+        boardRowElm.appendChild(boardCellElm)
+      }
+
+      this.elm.appendChild(boardRowElm)
     }
 
-    initGame() {
-        const {selector = "othella"} = this.options;
+    // this.handleCellClick(2, 3)();
+  }
 
-        this.elm = document.getElementById(selector);
+  reRender() {
+    for (let y = 0; y < this.options.width; y++) {
+      for (let x = 0; x < this.options.width; x++) {
+        const cell = this.board.getCell(x, y)
 
-        if (!this.elm)
-            throw new Error(`Element not found for id ${selector}`);
+        const boardCellElm = document.getElementById(`og-board-cell-${x}-${y}`)
+        boardCellElm.innerHTML = ''
+        boardCellElm.appendChild(
+          this.createNutElm(
+            cell.owner,
+            this.board.isValidMove(x, y),
+            this.board.turn,
+          ),
+        )
+      }
+    }
+  }
 
-        for (let y = 0; y < this.options.width; y++) {
-            const boardRowElm = this.createRowElm(y);
-            for (let x = 0; x < this.options.width; x++) {
+  createNutElm(type, validMove = false, turn) {
+    const nut = document.createElement('span')
+    nut.style.backgroundColor = type
+    nut.style.display = 'inline-block'
+    nut.style.width = `${Math.floor(this.options.cellMaxWidth * 0.8)}px`
+    nut.style.height = `${Math.floor(this.options.cellMaxWidth * 0.8)}px`
+    nut.style.borderRadius = '100%'
 
-                const cell = this.board.getCell(x, y);
-
-                const boardCellElm = this.createCellElm(x, y);
-                boardCellElm.appendChild(this.createNutElm(cell.owner, this.board.isValidMove(x, y), this.board.turn));
-
-                boardRowElm.appendChild(boardCellElm)
-            }
-
-            this.elm.appendChild(boardRowElm)
-        }
-
-        // this.handleCellClick(2, 3)();
-
+    if (validMove) {
+      nut.style.border = `1px dashed #${turn === 'white' ? 'eee' : '444'}`
     }
 
-    reRender() {
+    return nut
+  }
 
-        for (let y = 0; y < this.options.width; y++) {
-            for (let x = 0; x < this.options.width; x++) {
+  createRowElm(y) {
+    const elm = document.createElement('div')
+    elm.style.display = 'flex'
+    elm.setAttribute('class', `og-board-row row-${y}`)
+    elm.setAttribute('id', `og-board-row-${y}`)
 
-                const cell = this.board.getCell(x, y);
+    return elm
+  }
 
-                const boardCellElm = document.getElementById(`og-board-cell-${x}-${y}`);
-                boardCellElm.innerHTML = "";
-                boardCellElm.appendChild(this.createNutElm(cell.owner, this.board.isValidMove(x, y), this.board.turn));
+  handleCellClick(x, y) {
+    return () => {
+      if (this.board.finished) {
+        const result = this.board.gameResult()
+        console.log(result)
+        // create a div and put the result in it
+        const resultElm = document.createElement('div')
+        resultElm.setAttribute("id", "result")
+        resultElm.innerHTML = `Winner: ${result.winner} (${
+          result[result.winner]
+        })`
+        if (result.white === result.black) resultElm.innerHTML = `Equal!`
+        this.elm.appendChild(resultElm)
+        return
+      }
 
-            }
-        }
-
+      this.board.placeNutTo(x, y)
+      this.reRender()
     }
+  }
 
-    createNutElm(type, validMove = false, turn) {
-        const nut = document.createElement("span");
-        nut.style.backgroundColor = type;
-        nut.style.display = "inline-block";
-        nut.style.width = `${Math.floor(this.options.cellMaxWidth * .8)}px`;
-        nut.style.height = `${Math.floor(this.options.cellMaxWidth * .8)}px`;
-        nut.style.borderRadius = '100%';
+  createCellElm(x, y) {
+    const elm = document.createElement('div')
+    elm.setAttribute('class', `og-cell cell-${x}-${y}`)
+    elm.setAttribute('id', `og-board-cell-${x}-${y}`)
 
-        if (validMove) {
-            nut.style.border = `1px dashed #${turn === "white" ? "eee" : "444"}`;
-        }
+    elm.style.backgroundColor = this.options.cellBGColor
+    elm.style.border = '1px solid #d5d5d5'
+    elm.style.width = `${this.options.cellMaxWidth}px`
+    elm.style.height = `${this.options.cellMaxWidth}px`
+    elm.style.display = 'flex'
+    elm.style.justifyContent = 'center'
+    elm.style.alignItems = 'center'
 
-        return nut;
-    }
-
-    createRowElm(y) {
-        const elm = document.createElement("div");
-        elm.style.display = "flex";
-        elm.setAttribute("class", `og-board-row row-${y}`);
-        elm.setAttribute("id", `og-board-row-${y}`);
-
-        return elm;
-    }
-
-    handleCellClick(x, y) {
-        return () => {
-
-            if (this.board.finished) {
-                console.log(this.board.gameResult());
-                return;
-            }
-
-            this.board.placeNutTo(x, y);
-            this.reRender();
-        }
-    }
-
-    createCellElm(x, y) {
-        const elm = document.createElement("div");
-        elm.setAttribute("class", `og-cell cell-${x}-${y}`);
-        elm.setAttribute("id", `og-board-cell-${x}-${y}`);
-
-        elm.style.backgroundColor = this.options.cellBGColor;
-        elm.style.border = "1px solid #d5d5d5";
-        elm.style.width = `${this.options.cellMaxWidth}px`;
-        elm.style.height = `${this.options.cellMaxWidth}px`;
-        elm.style.display = "flex";
-        elm.style.justifyContent = "center";
-        elm.style.alignItems = "center";
-
-        elm.onclick = this.handleCellClick(x, y);
-        return elm;
-    }
-
+    elm.onclick = this.handleCellClick(x, y)
+    return elm
+  }
 }
 
-const OG = new OthelloGame();
+let cellMaxWidth = Math.min(
+  100,
+  (Math.min(window.innerWidth, window.innerHeight) - 100) / 8,
+)
+const OG = new OthelloGame({ cellMaxWidth })
